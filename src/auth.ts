f31dd7268c0b822d29c07c5ed16c61fecaae6087
Shinder22 <rushikeshshinde2205@gmail.com>
2025-04-26 16:49:27 +0530

    trust host

--- src/auth.ts
+++ src/auth.ts
@@ -3,5 +3,6 @@ import Google from "@auth/core/providers/google"
 import { SvelteKitAuth } from "@auth/sveltekit"
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
+        trustHost: true,
         providers: [Google],
 })
